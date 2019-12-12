dd7631cfae3e00ba812993cb56ff1832ac4c53c5
Mittens <44099251+MittensFFA@users.noreply.github.com>
2019-12-12 17:16:51 -0600

    Create images.py
    List of all image URL's

---
+++ images.py
@@ -0,0 +1,79 @@
+#role images
+imageURL_role_hitler = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/role_hitler.png?raw=true"
+imageURL_role_fascist = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/role_fascist.png?raw=true"
+imageURL_role_liberal = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/role_liberal.png?raw=true"
+
+#vote images
+imageURL_vote_ja = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/vote_ja.jpg?raw=true"
+imageURL_vote_nein = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/vote_nein.JPG?raw=true"
+
+#policy images
+imageURL_policy_fascist = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/policy_fascist.png?raw=true"
+imageURL_policy_liberal = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/policy_liberal.png?raw=true"
+
+#membership images
+imageURL_membership_fascist = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/membership_fascist.png?raw=true"
+imageURL_membership_liberal = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/membership_liberal.png?raw=true"
+
+#game board images
+#fascist board 0 policies enacted
+imageURL_board_fascist_0 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_fascist_0.png?raw=true"
+#fascist board 1 policy enacted
+imageURL_board_fascist_1 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_fascist_1.png?raw=true"
+#fascist board 2 policies enacted
+imageURL_board_fascist_2 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_fascist_2.png?raw=true"
+#fascist board 3 policies enacted
+imageURL_board_fascist_3 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_fascist_3.png?raw=true"
+#fascist board 4 policies enacted
+imageURL_board_fascist_4 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_fascist_4.png?raw=true"
+#fascist board 5 policies enacted
+imageURL_board_fascist_5 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_fascist_5.png?raw=true"
+#fascist board 6 policies enacted
+imageURL_board_fascist_6 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_fascist_6.png?raw=true"
+
+#liberal board 0 policies enacted & election tracker at 0
+imageURL_board_liberal_0_0 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_liberal_0_0.png?raw=true"
+#liberal board 0 policies enacted & election tracker at 1
+imageURL_board_liberal_0_1 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_liberal_0_1.png?raw=true"
+#liberal board 0 policies enacted & election tracker at 2
+imageURL_board_liberal_0_2 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_liberal_0_2.png?raw=true"
+#liberal board 0 policies enacted & election tracker at 3
+imageURL_board_liberal_0_3 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_liberal_0_3.png?raw=true"
+#liberal board 1 policy enacted & election tracker at 0
+imageURL_board_liberal_1_0 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_liberal_1_0.png?raw=true"
+#liberal board 1 policy enacted & election tracker at 1
+imageURL_board_liberal_1_1 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_liberal_1_1.png?raw=true"
+#liberal board 1 policy enacted & election tracker at 2
+imageURL_board_liberal_1_2 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_liberal_1_2.png?raw=true"
+#liberal board 1 policy enacted & election tracker at 3
+imageURL_board_liberal_1_3 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_liberal_1_3.png?raw=true"
+#liberal board 2 policies enacted & election tracker at 0
+imageURL_board_liberal_2_0 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_liberal_2_0.png?raw=true"
+#liberal board 2 policies enacted & election tracker at 1
+imageURL_board_liberal_2_1 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_liberal_2_1.png?raw=true"
+#liberal board 2 policies enacted & election tracker at 2
+imageURL_board_liberal_2_2 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_liberal_2_2.png?raw=true"
+#liberal board 2 policies enacted & election tracker at 3
+imageURL_board_liberal_2_3 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_liberal_2_3.png?raw=true"
+#liberal board 3 policies enacted & election tracker at 0
+imageURL_board_liberal_3_0 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_liberal_3_0.png?raw=true"
+#liberal board 3 policies enacted & election tracker at 1
+imageURL_board_liberal_3_1 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_liberal_3_1.png?raw=true"
+#liberal board 3 policies enacted & election tracker at 2
+imageURL_board_liberal_3_2 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_liberal_3_2.png?raw=true"
+#liberal board 3 policies enacted & election tracker at 3
+imageURL_board_liberal_3_3 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_liberal_3_3.png?raw=true"
+#liberal board 4 policies enacted & election tracker at 0
+imageURL_board_liberal_4_0 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_liberal_4_0.png?raw=true"
+#liberal board 4 policies enacted & election tracker at 1
+imageURL_board_liberal_4_1 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_liberal_4_1.png?raw=true"
+#liberal board 4 policies enacted & election tracker at 2
+imageURL_board_liberal_4_2 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_liberal_4_2.png?raw=true"
+#liberal board 4 policies enacted & election tracker at 3
+imageURL_board_liberal_4_3 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_liberal_4_3.png?raw=true"
+#liberal board 5 policies enacted & election tracker at 0
+imageURL_board_liberal_5_0 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_liberal_5_0.png?raw=true"
+#liberal board 5 policies enacted & election tracker at 1
+imageURL_board_liberal_5_1 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_liberal_5_1.png?raw=true"
+#liberal board 5 policies enacted & election tracker at 2
+imageURL_board_liberal_5_2 = "https://github.com/FFA-For-All/Secret-Hitler-Bot/blob/master/images/board_liberal_5_2.png?raw=true"
