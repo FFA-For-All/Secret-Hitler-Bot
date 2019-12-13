0ca4aa2887fc3e29b9a1af4b7e9314741ff1f87e
Mittens <44099251+MittensFFA@users.noreply.github.com>
2019-12-12 22:43:56 -0600

    Add files via upload

---
+++ SHBot.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 11 21:13:41 2019
+
+@author: benja
+"""
+
+# SHBot.py
+
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(token)
+
